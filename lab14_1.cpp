11f8868e37b901c31e453622b838e4da6d728727
TeetatB <teetatmix36108@gmail.com>
2024-01-05 15:50:57 +0700

    lab14_1 completed

--- lab14_1.cpp
+++ lab14_1.cpp
@@ -3,8 +3,23 @@ using namespace std;
 
 template <typename T>
 void insertionSort(T d[],int N){
-
+    int j;
+    for(int i = 1; i < N;i++){
+        T temp = d[i];
+        j = i-1;
+        while(j>=0 && d[j] < temp){
+            d[j+1] = d[j];
+            j = j-1;
+        }
+        d[j+1] = temp;
+        cout << "Pass " << i << ":";
+        for(int i = 0;i < N;i++){
+            cout << d[i] << " ";
+        }
+        cout << "\n";
+    }
 }
+
 
 int main(){
 	int a[10] = {12,25,30,44,2,0,4,7,55,25};
